2e45382f1576aa082aa64d31e1ce3cc31e975312
Mahdi <mahdilk84@gmail.com>
2025-04-06 19:32:29 +0330

    Start

---
+++ src/main/java/com/example/homepage/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.homepage;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 883, 458);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        stage.setTitle("HomePage");
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
